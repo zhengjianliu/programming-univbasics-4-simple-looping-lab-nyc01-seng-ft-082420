0b8e8b4158cf8ea1e30b1f72c9ce3c233430c7cb
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-11 22:36:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,35 @@
 # Write your methods here
+
+def loop_message_five_times(message)
+  i = 0
+  while i < 5 do
+    puts message
+    i += 1 
+  end
+end
+
+def loop_message_n_times(message,n)
+  i = 0
+  while  i!= n do
+    puts message
+    i += 1 
+  end
+end
+
+def output_array(array)
+  i = 0
+  while array[i] do
+    puts array[i]
+    i+=1
+  end
+end
+
+def return_string_array(array)
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array.push(array[count].to_s)
+    count += 1
+  end
+  new_array
+end
